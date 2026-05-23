3d4470c51c465512564c4815bf8210cdd491e9eb
carlos05sauceda <carlos05sauceda@gmail.com>
2026-05-22 23:58:46 -0600

    Building Interface

---
+++ src/types/Formulario.ts
@@ -0,0 +1,11 @@
+export interface Formulario {
+    nombre: string;
+    email: string;
+    Mensaje: string;
+}
+
+export interface ErroresFormulario {
+  nombre?: string;
+  email?: string;
+  Mensaje?: string;
+}
